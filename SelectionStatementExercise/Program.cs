f607343e6a8b78c223a2d4e0cbc56896c7b2cae3
RickStevenson <dadsathome1@gmail.com>
2024-09-06 18:13:47 -0600

    Commit first exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Xml;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random(); //This line creates a new `Random` object, which is used to generate random numbers.
+            int favNumber = r.Next(1, 1000);  //This line generates a random integer between 1 and 999 and assigns it to the `favNumber` variable.
+
+
+            Console.WriteLine("Guess my favorite number");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < favNumber)
+            {
+                Console.WriteLine("Guess is too low");
+            }
+            else if (guess > favNumber)
+            {
+                Console.WriteLine("Guess is too high");
+            }
+            else 
+            { 
+                Console.WriteLine("That's correct!"); 
+            }
+
+
+
+
         }
     }
 }
