ce19322e81dcaa5a22ce4829e5d04f8275ce6b81
RickStevenson <dadsathome1@gmail.com>
2024-09-09 12:19:35 -0600

    Added Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 
 namespace SelectionStatementExercise
 {
@@ -6,24 +7,70 @@ namespace SelectionStatementExercise
     {
         static void Main(string[] args)
         {
+            
+            FavNumber();
+            //SwitchExercis();
+        }
+
+        public static void FavNumber()
+        {
+            Random r = new Random();
+            int favNumber = r.Next(1, 21); // 1 to 20 inclusive
+            int guess = -1;
+            string answer = "";
+
+            Console.WriteLine("Guess my favorite number between 1 and 20");
+
+            do
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out guess))
+                {
+
+                    if (guess < 1 || guess > 20)
+                    {
+                        Console.WriteLine("Please guess a number between 1 and 20.");
+                    }
+                    else
+                    {
+                        answer = guess < favNumber ? "Too Low" :
+                                 guess > favNumber ? "Too High" :
+                                 "That's correct!";
+                        Console.WriteLine(answer);
+
+                        if (answer != "That's correct!")
+                        {
+                            Console.WriteLine("Guess again");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+
+            } while (answer != "That's correct!");
+        }
+        public static void SwitchExercis()
+        {
             Console.WriteLine("What is your favorite school subject?");
-            string favSubject = Console.ReadLine();
+            string favSubject = Console.ReadLine().ToLower();
 
             switch (favSubject)
             {
-                case "Math":
+                case "math":
                     Console.WriteLine("He likes math");
                     break;
 
-                case "History":
+                case "history":
                     Console.WriteLine("He likes history");
                     break;
 
-                case "Science":
+                case "science":
                     Console.WriteLine("He likes Science");
                     break;
 
-                case "Gym":
+                case "gym":
                     Console.WriteLine("He likes Gym");
                     break;
 
--- SelectionStatementExercise/obj/Debug/net6.0/SelectionStatementExercise.AssemblyInfo.cs
+++ SelectionStatementExercise/obj/Debug/net6.0/SelectionStatementExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f607343e6a8b78c223a2d4e0cbc56896c7b2cae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c52523765f12f4e7e88b1ff10b40238c397d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
